50375d5d8d4f298a25187cf1648c21f4a6e1d687
Vanillj <robindavidsson@outlook.com>
2022-05-01 11:36:50 +0200

    cleaned up

--- assignment1/assignment1/src/Chatterbot.hs
+++ assignment1/assignment1/src/Chatterbot.hs
@@ -27,7 +27,6 @@ type BotBrain = [(Phrase, [Phrase])]
 --------------------------------------------------------
 
 stateOfMind :: BotBrain -> IO (Phrase -> Phrase)
-{- TO BE WRITTEN -}
 stateOfMind brain =
   do
     r <- randomIO :: IO Float;
@@ -35,40 +34,15 @@ stateOfMind brain =
     return $ rulesApply list 
 
 rulesApply :: [PhrasePair] -> Phrase -> Phrase
-{- TO BE WRITTEN 
-rulesApply phrasepair phrase = 
-  let l = transformationsApply "*" phrase phrasepair
-  in case l of 
-    Just a -> a
-    Nothing -> phrase-}
--- try (transformationsApply "*" phrase phrasepair)
---try . transformationsApply "*" phrasepair phrase
 rulesApply phrasepair phrase =
   case word of
     Nothing -> words ""
     Just a -> a
   where word = (\i -> transformationsApply "*" id phrasepair i) (reflect phrase)
---concat [let l = transformationsApply "*" id phrasepair x 
-                                --in try l (Just []) --case l of 
-                                  --Just a -> a
-                                  --Nothing -> [] 
-                                  -- | x <- phrase]
-{-rulesApply phrasepair phrase = head [
-                                let list = head $ match "*" [x] (fst y) 
-                                in 
-                                  case list of 
-                                    Just a -> concat $ substitute "*" (snd y) a 
-                                    Nothing -> x | x <- phrase, y <- phrasepair]-}
 
 --"please help me" -> phrasepair -> första delen -> match första delen med INPUT -> 
-  --"please *" -> "help me" -> phrasepair -> andra delen -> "*" -> "help me"
-
-
--- rulesApply phrasepair phrase = [ | x <- phrase ]
-
-
+--"please *" -> "help me" -> phrasepair -> andra delen -> "*" -> "help me"
 reflect :: Phrase -> Phrase
-{- TO BE WRITTEN -}
 reflect phrase = [ let l = lookup x reflections
                     in case l of
                       Nothing -> x
@@ -107,12 +81,10 @@ prepare :: String -> Phrase
 prepare = reduce . words . map toLower . filter (not . flip elem ".,:;*!#%&|")
 
 rulesCompile :: [(String, [String])] -> BotBrain
-{- TO BE WRITTEN -}
 -- botbrain is structured as [(Phrase, [Phrase])]
 -- foreach element in input we create a pair where we use "map2" to apply "words . map toLower" to the first 
 -- element of the input pair to making it a Phrase and lowercase. The second element of the pair becomes 
 -- "map words (snd x)" which is a list of answers the robot can reply.
---rulesCompile xs = map (map2 (words . map toLower, map words)) xs
 rulesCompile xs = [(words (map toLower (fst x)), map words (snd x)) | x <- xs]
 
 --------------------------------------
@@ -136,9 +108,8 @@ reductions = (map.map2) (words, words)
 reduce :: Phrase -> Phrase
 reduce = reductionsApply reductions
 
-reductionsApply :: [PhrasePair] -> Phrase -> Phrase
-{- TO BE WRITTEN -}
 -- Using the function 'transformationsApply' we wrote earlier. Applies the function using fix for the values in xs and p.
+reductionsApply :: [PhrasePair] -> Phrase -> Phrase
 reductionsApply xs p = fix (\i -> case (transformationsApply "*" id xs i) of
                                   Nothing -> i
                                   Just a -> a) p
@@ -150,7 +121,6 @@ reductionsApply xs p = fix (\i -> case (transformationsApply "*" id xs i) of
 
 -- Replaces a wildcard in a list with the list given as the third argument
 substitute :: Eq a => a -> [a] -> [a] -> [a]
-{- TO BE WRITTEN -}
 substitute _ [] _ = []
 substitute a (x:xs) ys
   | a == x = ys ++ substitute a xs ys
@@ -159,11 +129,9 @@ substitute a (x:xs) ys
 -- Tries to match two lists. If they match, the result consists of the sublist
 -- bound to the wildcard in the pattern list.
 match :: Eq a => a -> [a] -> [a] -> Maybe [a]
--- match _ _ _ = Nothing
 match _ [] [] = Just []
 match _ [] _ = Nothing
 match _ _ [] = Nothing
-{- TO BE WRITTEN -}
 match wildcard p s
   -- if they're equal, continue searching
   | pi == si = match wildcard (tail p) (tail s)
@@ -177,7 +145,6 @@ match wildcard p s
 
 -- Helper function to match
 singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-{- TO BE WRITTEN -}
 -- we know that x is the answer as the first correct element since 
 -- match call this when pi == wildcard. This will always return 
 -- Just [x], or nothing of course if there is no matches
@@ -187,15 +154,13 @@ singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
 singleWildcardMatch (wc:ps) (x:xs) = match wc ps xs >> Just [x]
 
 
-{- TO BE WRITTEN -}
 -- appends the correct x (using (x:)) and then continues looking for new
 -- values AFTER x that are not equal to the elements in wc:ps
 -- for example, "a=*;" "a=32;", first we get x = 3, then we continue
 -- looking but in the match loop pi == si will not be equal but pi == wildcard
 -- will be equal so that means now 2 will also be appended, thus we have 32. 
 -- next iteration will reach the ; and result in pi == si being true finaly reaching Nothing
-longerWildcardMatch (wc:ps) (x:xs) =
-  mmap (x:) $ match wc (wc:ps) xs
+longerWildcardMatch (wc:ps) (x:xs) = mmap (x:) $ match wc (wc:ps) xs
 
 
 -- findInd '*' "a=*;" $ reverse $ findInd '*' "a=*;" "a=32;"
@@ -217,37 +182,12 @@ matchCheck = matchTest == Just testSubstitutions
 
 -- Applying a single pattern
 transformationApply :: Eq a => a -> ([a] -> [a]) -> [a] -> ([a], [a]) -> Maybe [a]
-{- TO BE WRITTEN -}
 transformationApply _ _ [] _ = Nothing
 transformationApply _ _ _ ([],_) = Nothing
 transformationApply _ _ _ (_,[]) = Nothing
 transformationApply b f xs (ys, zs) = mmap (substitute b zs) (match b ys xs)
-  {-case rep of
-    Just c -> Just $ substitute b zs c
-    Nothing -> Nothing
-  where rep = match b ys xs-}
 
 -- Applying a list of patterns until one succeeds
 transformationsApply :: Eq a => a -> ([a] -> [a]) -> [([a], [a])] -> [a] -> Maybe [a]
-{- TO BE WRITTEN
-transformationsApply b f pair xs
-  | length fullList > 0 = Just fullList
-  | otherwise = Nothing
-  where fullList = concat [ 
-          let l = transformationApply b f xs x
-          in case l of 
-            Just c -> c
-            Nothing -> [] | x <- pair ] -}
 transformationsApply _ _ [] _ = Nothing
 transformationsApply b f (p:pairs) xs = orElse (transformationApply b f xs p) (transformationsApply b f pairs xs)
-  --in case l of 
-    --Just a -> Just a
-    --Nothing -> transformationsApply b f pairs xs
-
-{- 
-helpFunc b f [] x = []
-helpFunc b f (c:xs) x =
-  case l of
-    Just a -> head a:helpFunc b f xs x
-    Nothing -> []:helpFunc b f xs x
-  where l = concatMap (a -> [b]) (t a) transformationApply b f (c:xs) x-}
